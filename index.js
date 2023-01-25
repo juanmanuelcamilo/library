11dfd232b0d4601153f1b74051c12c88fbce5f8f
Juan Manuel <j_m_c_27@hotmail.com>
2023-01-25 10:11:15 -0300

    Now the modal is hidden and  can be opened and closed with a button

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const addBookButton = document.querySelector(".add-book-btn");
+const modal = document.querySelector(".modal-container");
 
+//Open the Modal
+addBookButton.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+
+//Close the modal when clicking outside of it
+window.addEventListener("click", function (e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
