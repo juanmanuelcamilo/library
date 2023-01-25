1ee74330139fef9e3a8d4cdd03a750dd53991779
Juan Manuel <j_m_c_27@hotmail.com>
2023-01-25 10:36:19 -0300

    Adding the functionality of showing the books in the page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const addBookButton = document.querySelector(".add-book-btn");
 const modal = document.querySelector(".modal-container");
+const booksGrid = document.querySelector(".books-grid");
 
 //Open the Modal
 addBookButton.addEventListener("click", function () {
@@ -12,3 +13,44 @@ window.addEventListener("click", function (e) {
     modal.style.display = "none";
   }
 });
+
+//Array to store my books
+let myLibrary = [
+  { Title: "hasjkfas", Author: "fjhasjfkhjask", Pages: 20, Read: "Yes" },
+  { Title: "hasjkfas", Author: "fjhasjfkhjask", Pages: 20, Read: "Yes" },
+];
+
+//Object constructor
+class Book {
+  constructor(Title, Author, Pages, Read) {
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+    this.Read = Read;
+  }
+}
+
+//Add a book to the Array
+function addBookToLibrary() {
+  let book = new Book(Title, Author, Pages, Read);
+  myLibrary.push(book);
+}
+
+//Displaying the books on the Page
+function displayBooksOnPage() {
+  myLibrary.forEach((book) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    booksGrid.appendChild(card);
+
+    for (let key in book) {
+      console.log(`${key}: ${book[key]}`);
+      const para = document.createElement("p");
+      para.textContent = `${key}: ${book[key]}`;
+      card.appendChild(para);
+    }
+  });
+}
+
+displayBooksOnPage();
+console.log(myLibrary);
